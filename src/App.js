299b8f244180452c1950989d3ee50ff2d41c7205
Alpha Diallo <DRAGON VERT@DESKTOP-DE9PHME>
2021-12-14 12:11:41 +0000

    PROJET JSX

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './style.css';
+import imag from './images/doom3.jpg'
+import video from './samurai.mp4'
+
+const Style = {
+  border: "1px solid black",
+  textAlign:"center",
+  dispplay:"flex",
+  flexWrap:"wrap"
+
+}
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <>
+  <div style={Style}>
+    <h1 >Doom-Slayer</h1>
+    <br/>
+    <img  className='title red' src='../doom1.jpg' alt='doom1.jpg'/>
+    <br/>
+    <img className='title red' src={imag} alt='doom4.jpg'/>
+    <br/>
+    <video className='title red' width="320" height="240" controls>
+    <source src={video} type="video/mp4" />
+    </video>
+  
+  </div>
+  </>
+  )
 }
+
+
 
 export default App;
